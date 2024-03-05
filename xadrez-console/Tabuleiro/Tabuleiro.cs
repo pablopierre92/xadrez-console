c259f837e5e4cfc4682a352078fb6785c6ee1281
pablopierre92 <pablo_chips@hotmail.com>
2024-03-05 21:11:31 +0000

    Testando excecao e criando classe PosicaoXadrez

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -1,5 +1,7 @@
 ﻿
 
+using System.Diagnostics.SymbolStore;
+
 namespace tabuleiro
 {
     internal class Tabuleiro
@@ -21,10 +23,38 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao posicao) //sobrecarga
+        {
+            return pecas[posicao.linha, posicao.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
             pecas[pos.linha, pos.coluna] = p;   
             p.posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha < 0 || pos.linha >= linhas || pos.coluna < 0 || pos.coluna >= colunas)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if(!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição Inválida!");
+            }
         }
     }
 }
--- xadrez-console/Tabuleiro/TabuleiroException.cs
+++ xadrez-console/Tabuleiro/TabuleiroException.cs
@@ -4,9 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace xadrez_console.tabuleiro
+namespace tabuleiro
 {
-    internal class TabuleiroException
+    internal class TabuleiroException : Exception
     {
+        public TabuleiroException(string msg) : base(msg)
+        {
+
+
+        }
     }
 }
