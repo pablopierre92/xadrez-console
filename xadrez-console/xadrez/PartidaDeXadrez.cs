18e3437e9eaf512a1b50bbafd9bc7bfc622d03b1
pablopierre92 <pablo_chips@hotmail.com>
2024-03-13 22:29:34 +0000

    jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -152,6 +152,24 @@ namespace xadrez
                 throw new TabuleiroException("Você nao pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // # jogadaespecial promocao
+            if (p is Peao)
+            {
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -172,7 +190,7 @@ namespace xadrez
                 mudaJogador();
             }
 
-            Peca p = tab.peca(destino);
+            
 
             // # jogadaespecial em passant
             if (p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2))
